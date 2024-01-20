903cb0094d81ccb4a9ff53b95e8c2e697ff7d6cb
Marius L <mariuslacatus@gmail.com>
2024-01-20 09:32:50 -0500

    Initial Commit

---
+++ MNoise/MNoiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MNoiseApp.swift
+//  MNoise
+//
+//  Created by Marius L on 1/20/24.
+//
+
+import SwiftUI
+
+@main
+struct MNoiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
